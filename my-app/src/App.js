a98940c040f27dc312c28b6bb976d5db6de56b5d
KDW <ksj0740@gmail.com>
2025-10-11 17:49:10 +0900

    Modify app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd [HELLO!!]
+          Learn GithubAction cicd [HI!!]
         </a>
       </header>
     </div>
